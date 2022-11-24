5577cd30030a815a5ebdf72ddf4bfce29df6472d
Antra  Gupta <115134630+antra1947@users.noreply.github.com>
2022-11-24 14:04:37 +0530

    Add files via upload

---
+++ week-1-cipherschool/5th topic/variable.py
@@ -0,0 +1,23 @@
+#variable.py
+
+number1=4
+print(number1)
+number1= 7
+print(number1)
+
+name = 'antra'
+print(name)
+
+#rule 1 not doing
+#1number = 4 not doing
+#instead we start ,letter.....first letterI
+_name = "Antra"
+a1ntra = 18
+print(a1ntra)
+
+#snake case writing used in python
+user_one_name = "Antra"
+print(user_one_name)
+
+#camel case writing but mostly it is used in java
+userOneName = "Antra"
